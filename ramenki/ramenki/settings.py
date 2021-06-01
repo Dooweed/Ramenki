5a4c94925091577da90b0f389a34dec9fad2cbd3
David Alkamyan <59831958+Dooweed@users.noreply.github.com>
2021-06-01 14:51:31 +0500

    Update settings.py

--- ramenki/ramenki/settings.py
+++ ramenki/ramenki/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=asn27y@5t&lcu943md3=be0x0%$notob+gbbym%d^q2!vzl%s'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
